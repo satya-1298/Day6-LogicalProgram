11e6976de7edae6a22ea45fc72b0aedbf4ea4751
unknown <satya-1298>
2023-06-23 18:05:04 +0530

    [Satya]Added-Prime number program

--- LogicalProgram/Program.cs
+++ LogicalProgram/Program.cs
@@ -16,8 +16,11 @@ namespace LogicalProgram
                 Console.WriteLine("Welcome to Logical Programs");
                 
                 Console.WriteLine("1-FibonacciSeries");
+                Console.WriteLine("2-PerfectNumber");
+                Console.WriteLine("3-PrimeNumber");
                 {
-                    Console.WriteLine("CHOOSE THE BELOW OPTIONS");
+                    Console.WriteLine("CHOOSE THE ABOVE OPTIONS");
+
                     int option = Convert.ToInt32(Console.ReadLine());
                     switch (option)
                     {
@@ -25,6 +28,16 @@ namespace LogicalProgram
                             Console.WriteLine("Fibonacci Series Program");
                             Fibonacci fibonacci = new Fibonacci();
                             fibonacci.Series();
+                            break;
+                        case 2:
+                            Console.WriteLine("Perfect Number program");
+                            Perfect perfect = new Perfect();
+                            perfect.Numbers();
+                            break;
+                        case 3:
+                            Console.WriteLine("Prime Number Program");
+                            Prime prime = new Prime();
+                            prime.Number();
                             break;
                         default:
                             Console.WriteLine("Invalid Option");
